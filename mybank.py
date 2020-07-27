d48ef98363e08f3b016176174b4ac3f7fd218780
Sanpreet Kaur <52909190+sanpreetkaur-13@users.noreply.github.com>
2020-07-27 23:42:25 +0530

    work on bank dataset

--- mybank.py
+++ mybank.py
@@ -6,8 +6,9 @@ dataset= pd.read_csv('desktop/bank-additional.csv', na_values ='?' , sep=';')
 
 dataset.isnull().sum()
 
-X=dataset.iloc[:,0:20]
 y=dataset.iloc[:,-1]
+X=dataset.iloc[:,0:20]
+
 
 X_int=X.iloc[:,[0,10,11,12,13,15,16,17,18,19]]
 X_str=X.iloc[:,[1,2,3,4,5,6,7,8,9,14]]
@@ -21,5 +22,7 @@ X_final=np.concatenate([X_int,X_str],axis=1)
 
 from sklearn.preprocessing import LabelEncoder
 lab=LabelEncoder()
+
 y=lab.fit_transform(y)
+
 lab.classes_
